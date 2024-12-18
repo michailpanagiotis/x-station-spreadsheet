e05e3779e68239e7979e79d1b0346862fee7780f
Panos Michail <michailpanagiotis@gmail.com>
2024-12-18 10:35:53 +0200

    WIP

--- parser.py
+++ parser.py
@@ -158,10 +158,11 @@ indices = [
 ]
 
 class NumericValue(dict):
-    def __init__(self, name, byte):
+    def __init__(self, name, byte, aliases=[]):
         self.name = name
         self._byte = byte
         self._bytes = [byte]
+        self._aliases = aliases
         dict.__init__(self, name=name, byte=byte)
 
     def __repr__(self):
@@ -169,10 +170,6 @@ class NumericValue(dict):
 
     def __len__(self):
         return 1
-
-class SysexValue(NumericValue):
-    def __repr__(self):
-        return 'sx'
 
 class ZeroPadding():
     def __init__(self, name, num_bytes):
@@ -186,36 +183,45 @@ class ZeroPadding():
     def __len__(self):
         return self._num_bytes
 
-class BitMap(dict):
+class BitMap():
     def __init__(self, ms_name, ls_name, byte):
         self._ms_name = ms_name
         self._ls_name = ls_name
-        self._byte = byte
         self._bytes = bytes([byte])
         self.name = '%s|%s' % (ms_name, ls_name)
-        dict.__init__(self, ms_name=ms_name, ls_name=ls_name, byte=byte)
 
     def __repr__(self):
-        formatted = "{:08b}".format(self._byte)
+        formatted = "{:08b}".format(self._bytes[0])
         return '<%s>%s|<%s>%s' % (self._ms_name, formatted[:4], self._ls_name, formatted[4:])
 
     def __len__(self):
         return 1
 
-class StringValue(dict):
+class SysexValue():
     def __init__(self, name, value):
         self.name = name
+        if len(value) != 18:
+            raise Exception('bad length')
         self._bytes = bytes(value)
-        dict.__init__(self, name=name, bytes=value)
 
     def __repr__(self):
-        return '<%s>%s' % (self.name, ascii(self._bytes))
+        return '<%s>%s' % (self.name, ''.join([hex(x) for x in self._bytes]))
+
+class StringValue():
+    def __init__(self, name, value):
+        self.name = name
+        if len(value) != 16:
+            raise Exception('bad length')
+        self._bytes = bytes(value)
+
+    def __repr__(self):
+        return '<%s>%s' % (self.name, ''.join([chr(x) for x in self._bytes]).strip())
 
     def __len__(self):
         return len(self._bytes)
 
 class SingleControl(dict):
-    def __init__(self, idx, position, cmd, name_length=16):
+    def __init__(self, idx, position, cmd, name_length=16, sysex_length=18):
         if len(cmd) != 52:
             raise Exception('bad length')
 
@@ -223,41 +229,27 @@ class SingleControl(dict):
 
         fields = []
         name = bytes(cmd[:name_length])
-        fields.append(StringValue('name', name))
-
+        fields.append(StringValue('Control name', name))
         cmd = cmd[name_length:]
 
-        fields.append(NumericValue('Control Type', cmd.pop(0)))
-        fields.append(NumericValue('Low|Template|Velocity|MMC Command', cmd.pop(0)))
-        fields.append(NumericValue('Hi', cmd.pop(0)))
+        fields.append(NumericValue('Type', cmd.pop(0), aliases=['Control Type']))
+        fields.append(NumericValue('Low', cmd.pop(0), aliases=['Template', 'Velocity', 'MMC Command']))
+        fields.append(NumericValue('High', cmd.pop(0)))
         fields.append(BitMap('Ports', 'Button type', cmd.pop(0)))
-        fields.append(NumericValue('PotCtrl', cmd.pop(0)))
-        fields.append(NumericValue('DisplayType', cmd.pop(0)))
-        fields.append(NumericValue('NRPN|RPN MSBank Num', cmd.pop(0)))
-        fields.append(NumericValue('CC|Note', cmd.pop(0)))
-        fields.append(NumericValue('Channel|Device id', cmd.pop(0)))
-        fields.append(NumericValue('Template|Velocity|MMC Command', cmd.pop(0)))
+        fields.append(NumericValue('Pot Type', cmd.pop(0)))
+        fields.append(NumericValue('Display Type', cmd.pop(0)))
+        fields.append(NumericValue('MSBank', cmd.pop(0), aliases=['NRPN MSBank Num']))
+        fields.append(NumericValue('CC', cmd.pop(0), aliases=['Note']))
+        fields.append(NumericValue('Ch', cmd.pop(0), aliases=['Channel', 'Device id']))
+        fields.append(NumericValue('Template', cmd.pop(0), aliases=['Template', 'Velocity', 'MMC Command']))
         fields.append(NumericValue('unknown1', cmd.pop(0)))
         fields.append(NumericValue('unknown2', cmd.pop(0)))
         fields.append(NumericValue('unknown3', cmd.pop(0)))
-        fields.append(SysexValue('sx1', cmd.pop(0)))
-        fields.append(SysexValue('sx2', cmd.pop(0)))
-        fields.append(SysexValue('sx3', cmd.pop(0)))
-        fields.append(SysexValue('sx4', cmd.pop(0)))
-        fields.append(SysexValue('sx5', cmd.pop(0)))
-        fields.append(SysexValue('sx6', cmd.pop(0)))
-        fields.append(SysexValue('sx7', cmd.pop(0)))
-        fields.append(SysexValue('sx8', cmd.pop(0)))
-        fields.append(SysexValue('sx9', cmd.pop(0)))
-        fields.append(SysexValue('sx10', cmd.pop(0)))
-        fields.append(SysexValue('sx11', cmd.pop(0)))
-        fields.append(SysexValue('sx12', cmd.pop(0)))
-        fields.append(SysexValue('sx13', cmd.pop(0)))
-        fields.append(SysexValue('sx14', cmd.pop(0)))
-        fields.append(SysexValue('sx15', cmd.pop(0)))
-        fields.append(SysexValue('sx16', cmd.pop(0)))
-        fields.append(SysexValue('sx17', cmd.pop(0)))
-        fields.append(SysexValue('sx18', cmd.pop(0)))
+
+        sysex = bytes(cmd[:sysex_length])
+        fields.append(SysexValue('Sysex', sysex))
+        cmd = cmd[sysex_length:]
+
         fields.append(NumericValue('Step', cmd.pop(0)))
         fields.append(ZeroPadding('Padding', 4))
 
@@ -267,7 +259,7 @@ class SingleControl(dict):
             raise Exception('non parsed fields')
 
         self._idx = idx
-        self._position = position
+        self.position = position
         self.name = name
         self._fields = fields
         for field in fields:
@@ -327,7 +319,7 @@ class Template():
 
     def print_all(self):
         for control in self.controls:
-            print(control._position, control._position + len(control), control['name'], control)
+            print(control.position, control.position + len(control), control.name, control)
 
     def print_distinct(self, fieldName):
         print(set([c[fieldName] for c in self.controls]), sys.argv[1])
@@ -345,8 +337,8 @@ class Template():
         return _bytes
 
 template = Template(sys.argv[1])
-# template.print_all()
-template.print_fields('unknown3')
+template.print_all()
+# template.print_fields('unknown3')
 # template.print_distinct('unknown3')
 # print(template._bytes)
 # template.write(sys.argv[2])
